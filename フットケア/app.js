3b95c0a9bc7f26ac9d018ba81a8cbf8592ee5f4a
tomomz36 <tomosukemk1@gmail.com>
2024-09-07 22:24:34 +0900

    Add files via upload

---
+++ フットケア/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mysql = require('mysql2');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  root:'3306',
+  user: 'memon',
+  password: 'memo1234',	
+  database: 'logintry'
+});
